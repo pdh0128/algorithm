bd6e8ffb5b8e31107935f03357a5b4c854436c0b
pdh0128 <162246145+pdh0128@users.noreply.github.com>
2024-08-02 16:02:26 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    int k;
+    scanf("%d %d", &N, &k);
+    int student[N];
+    for(int i = 0; i < N; ++i) {
+        scanf("%d", &student[i]);
+    }
+    for(int i = 0; i < N - 1; ++i) {
+        int min = i;
+        for(int j = i + 1; j < N; ++j) {
+            if (student[j] > student[min]) min = j;
+        }
+        int temp = student[i];
+        student[i] = student[min];
+        student[min] = temp;
+    }
+    printf("%d", student[k - 1]);
+    return 0;
+}
