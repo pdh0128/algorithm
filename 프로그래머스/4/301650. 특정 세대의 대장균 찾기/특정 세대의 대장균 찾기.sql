95d118275d4a48a83c4bf8600d2327a28c7f3aa5
ComodoPdh <162246145+pdh0128@users.noreply.github.com>
2024-09-21 13:46:25 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+select ID 
+from ECOLI_DATA 
+where PARENT_ID in 
+(select ID from ECOLI_DATA where PARENT_ID in 
+(select ID from ECOLI_DATA where PARENT_ID is NULL))
+
+order by ID asc;
