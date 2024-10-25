de88bb3a5a43a1edc1f822c8ac453d13bf3ff50f
ComodoPdh <162246145+pdh0128@users.noreply.github.com>
2024-10-25 19:57:59 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, case when STATUS = "SALE" then "판매중" when STATUS = "RESERVED" then "예약중" else "거래완료" end as STATUS from USED_GOODS_BOARD where CREATED_DATE = "2022-10-5" order by BOARD_ID desc;
