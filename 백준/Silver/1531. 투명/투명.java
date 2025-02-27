bdeb76fc625c2236dc84d560b8da36e0d98e4289
Comodo <162246145+pdh0128@users.noreply.github.com>
2025-02-28 00:56:22 +0900

    [Silver V] Title: 투명, Time: 200 ms, Memory: 18060 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[][] matrix = new int[100][100];
+    for(int i = 0; i < 100; ++i) {
+      for(int j = 0; j < 100; ++j) {
+        matrix[i][j] = 0;
+      }
+    }
+    int cnt = 0;
+    for(int i = 0; i < n; ++i) {
+      int lx = sc.nextInt();
+      int ly = sc.nextInt();
+      int rx = sc.nextInt();
+      int ry = sc.nextInt();
+      for(int j = lx - 1; j < rx; ++j) {
+        for(int k = ly - 1; k < ry; ++k) {
+          matrix[j][k] += 1;
+        }
+      }
+    }
+    for(int i = 0; i < 100; ++i) {
+      for(int j = 0; j < 100; ++j) {
+        if (matrix[i][j] > m) ++cnt;
+      }
+    }
+    System.out.println(cnt);
+  }
+}
